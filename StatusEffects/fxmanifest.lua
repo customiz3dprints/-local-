98d234f2b4e336906dd806e57929b44894f011a8
customiz3dprints <hestegimposztor2@gmail.com>
2025-06-09 17:00:23 +0200

    Status Effect system: initial commit

---
+++ StatusEffects/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'DDani6'
+description 'A simple status effect system with hunger and thirst.'
+version '1.1.0'
+
+client_script {'client/client.lua'}
+server_script { '@oxmysql/lib/MySQL.lua'}
+shared_script '@ox_lib/init.lua'
+
+ui_page 'nui/nui.html'
+files {
+    'nui/nui.html',
+    'nui/nui.js',
+    'nui/nui.css',
+}
